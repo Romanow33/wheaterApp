bc653359f40d13c2018e913f8d35e70870d5adc9
Ignacio Romanwow <romanowignacio1@gmail.com>
2021-08-04 21:33:03 -0300

    commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
-import './input.css'
+import './SearchBar.css'
 
 export default function SearchBar({onSearch}) {
   const [city, setCity] = useState("");
   return (
+    
+    <div class="search-box">
     <form onSubmit={(e) => {
       e.preventDefault();
       onSearch(city);
     }}>
-      <input className='search'
+      <button type="submit" className ="btn-search">
+      <img src="" alt="" />
+      </button>
+      <input className='input-search'
         type="text"
-        placeholder="Ciudad..."
+        placeholder="BUSCAR"
         value={city}
         onChange={e => setCity(e.target.value)}
       />
-      <button type="submit" className ="btn">Agregar</button>
+      
+
       {/* <input type="submit" value="Agregar" /> */}
     </form>
+    </div>
   );
 }
